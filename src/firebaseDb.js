646800a98ed2803a2785ed27ab326602d8d5d34d
Digamber Singh <digamber@positronx.io>
2020-04-24 09:29:59 +0530

    first commit

---
+++ src/firebaseDb.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id"
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'add',
+    component: () => import('../components/UserCreate')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/list',
+    name: 'list',
+    component: () => import('../components/UserList')
+  },
+  {
+    path: '/edit/:id',
+    name: 'edit',
+    component: () => import('../components/UserEdit')
   }
 ]
 
